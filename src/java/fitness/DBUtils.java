eef7546346903e6e725f01ade3be48bd3f19919b
Chance Cooper <c0659824@mylambton.ca>
2017-04-03 10:45:24 -0400

    Added a hash and salt.

--- src/java/fitness/DBUtils.java
+++ src/java/fitness/DBUtils.java
@@ -1,5 +1,8 @@
-package midterm;
+package fitness;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -18,13 +21,35 @@ public class DBUtils {
      * @return the Connection object
      * @throws SQLException
      */
+    public final static String SALT = "Gjs#ka89sk4klaksl@#laS12";
+    
+    public static String hash(String password) {
+        try {
+            String salted = password + SALT;
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] hash = md.digest(salted.getBytes("UTF-8"));
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(b & 0xff).toUpperCase();
+                if (hex.length() == 1) {
+                    sb.append("0");
+                }
+                sb.append(hex);
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            Logger.getLogger(DBUtils.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(DBUtils.class.getName()).log(Level.SEVERE, null, ex);
         }
-        String server = "ipro.lambton.on.ca";
+        String server = "35.185.105.230";
         String username = "root";
         String password = "muttonchops987";
         String jdbc = String.format("jdbc:mysql://%s/%s", server, "fitness");
